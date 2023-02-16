d5ba1dae8fd3772975e39b32ed01a10812a804a6
Dinesh Jothiram <dinesh.jothiram@insofe.edu.in>
2023-02-16 11:16:24 +0530

    Changed testsize 4

--- model.py
+++ model.py
@@ -8,7 +8,7 @@ df=pd.read_csv('diabetes.csv')
 X=df.iloc[:,:-1]
 Y=df.iloc[:,-1]
 
-X_train,X_test,y_train,y_test=train_test_split(X,Y,test_size=0.7,random_state=2)
+X_train,X_test,y_train,y_test=train_test_split(X,Y,test_size=0.7,random_state=3)
 
 classifier=RandomForestClassifier()
 classifier.fit(X_train,y_train)
